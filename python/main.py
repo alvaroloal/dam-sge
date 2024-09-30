e7b1d8db616bbc4a48a7907135bb8d8c30afe73c
alvaroloal <alorentealman@gmail.com>
2024-09-30 13:51:59 +0200

    ejemplos python

--- python/main.py
+++ python/main.py
@@ -49,8 +49,215 @@ palabra = "Python"
 len(palabra)
 
 ##listas
+numeros = [1,2,3,4]
+print (numeros)
+
+##listas anidadas
+a = [1,2,3]
+b = [4,5,6]
+c = [7,8,9]
+r = [a,b,c]
+print(r)
+##para acceder a la posicio del numero 3
+print(r[0][2])##accedo al elemento grande y luego al pequeño
+
+print(r[0][1:3])##accedo a las posiciones segunda y tercera
+
+print(r[0])       # Primera sublista
+print(r[-1])      # Última sublista
+
+print(r[0][0])    # Primera sublista, y de ella, primer ítem
+print(r[1][1])    # Segunda sublista, y de ella, segundo ítem
+print(r[2][2])    # Tercera sublista, y de ella, tercer ítem
+print(r[-1][-1])  # Última sublista, y de ella, último ítem
 
 
 
+# valor = input("Introduce un valor: ")  # Podemos mostrar un mensaje
+# valor
 
+# valor = int(input("Introduce un número entero: "))
+# valor * 2
+
+##Condicionales
+nota = float(input("Introduce una nota: "))
+
+if nota >= 9:
+    print("Sobresaliente")
+if nota >= 7 and nota < 9:
+    print("Notable")
+if nota >= 6 and nota < 7:
+    print("Bien")
+if nota >= 5 and nota < 6:
+    print("Suficiente")
+if nota < 5:
+    print("Insuficiente")
+
+
+##Sirve para como instrucción de paso para utilizar en un bloque de código vacío, no finaliza el código. No tiene ningún efecto pero sirve para crear estructuras pendientes de ser programadas
+if True:
+    pass
+
+
+##while
+c = 0
+while c <= 5:
+    c+=1
+    print("c vale", c)
+else:
+    print("Se ha completado toda la iteración y c vale", c)
+
+
+##break
+c = 0
+while c <= 5:
+    c+=1
+    if (c==4):
+        print("Rompemos el bucle cuando c vale", c)
+        break
+    print("c vale",c)
+else:
+    print("Se ha completado toda la iteración y c vale", c)
+    
+#     ##Ejemplo menú interactivo
+#     print("Bienvenido al menú interactivo")
+# while(True):
+#     print("""¿Qué quieres hacer? Escribe una opción
+#     1) Saludar
+#     2) Sumar dos números
+#     3) Salir""")
+#     opcion = input()
+#     if opcion == '1':
+#         print("Hola, espero que te lo estés pasando bien")
+#     elif opcion == '2':
+#         n1 = float(input("Introduce el primer número: "))
+#         n2 = float(input("Introduce el segundo número: "))
+#         print("El resultado de la suma es: ",n1+n2)
+#     elif opcion =='3':
+#         print("¡Hasta luego! Ha sido un placer ayudarte")
+#         break
+#     else:
+#         print("Comando desconocido, vuelve a intentarlo")
+
+##for con listas
+numeros = [1,2,3,4,5,6,7,8,9,10]
+indice = 0
+while indice < len(numeros):
+    print(numeros[indice])
+    indice+=1
+
+for numero in numeros:  # Para [variable] en [lista]
+    print(numero)
+
+
+##for con cadenas
+cadena = "Hola amigos"
+for caracter in cadena:
+    print(caracter)
+
+
+
+##Para asignar un nuevo valor a los elementos de una lista mientras la recorremos, podríamos intentar asignar al número el nuevo valor:
+for numero in numeros:
+    numero *= 10   
+numeros
+
+
+##enumerate 
+##Podemos utilizar la función enumerate() para conseguir el índice y el valor en cada iteración fácilmente:
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for indice,numero in enumerate(numeros):
+    numeros[indice] *= 10
+print (numeros)
+
+##for con cadenas
+cadena = "Hola amigos"
+for caracter in cadena:
+    print(caracter)
+
+
+##Funcion range
+for i in range(10):
+    print(i) 
+
+list(range(10))
+
+
+##Tuplas
+tupla = (100,"Hola",[1,2,3],-50)
+print (tupla)
+
+print(tupla)
+print(tupla[0])
+print(tupla[-1])
+print(tupla[2:])
+print(tupla[2][-1])
+
+
+##longuitud de la tupla
+len(tupla)
+
+
+##para buscar un elemento y saber su posición en la tupla:
+tupla.index(100)
+tupla.index('Hola')
+
+## para contar cuantas veces aparece un elemento en una tupla:
+tupla.count(100)
+
+tupla = (100,100,100,50,10)
+tupla.count(100)
+
+##las tuplas no disponen de métodos para modificar su contenido:
+tupla.append(10)
+##da error
+
+
+##Conjuntos
+
+
+##Para definir un conjunto vacío hay que llamar a su clase set (conjunto en inglés):
+conjunto = set()
+conjunto
+
+##si lo creamos con algunos datos se definen entre llaves:
+conjunto = {1,2,3}
+conjunto
+
+##add
+##Sirve para añadir elementos al conjunto, pero si un elemento ya se encuentra no se añadirá de nuevo:
+conjunto.add(4)
+conjunto
+
+##gestionan automáticamente la posición de sus elementos, en lugar de conservarlos en la posición que nosotros los añadimos:
+conjunto.add('H')
+conjunto.add('A')
+conjunto.add('Z')
+conjunto
+
+##Pertenencia a grupos
+grupo = {'Hector','Juan','Mario'}
+
+##Es fácil saber si un elemento se encuentra en un conjunto utilizando la sintaxis in. Se utiliza mucho para trabajar con grupos:
+'Hector' in grupo
+'Hector' not in grupo
+
+##Diccionarios
+## se definen igual que los conjuntos, utilizando llaves, pero también se pueden crear vacíos con ellas:
+vacio = {}
+vacio
+
+##Si consultamos el tipo de la variable que contiene un diccionario con la función type() encontraremos la palabra dict, esa es la clase que define los diccionarios:
+type(vacio)
+
+
+##Definicion diccionario
+##Para cada elemento se define la estructura clave:valor:
+colores = {'amarillo':'yellow','azul':'blue'}
+colores
+
+##Para consultar el valor de una clave utilizaremos la clave a modo de índice:
+colores['amarillo']
+
+##Mutabilidad
 
