55be061728ff79f3b7887f3f3b8c73eb6bb6014d
alvaroloal <alorentealman@gmail.com>
2024-09-30 09:06:30 +0200

    ejemplo python

---
+++ python/main.py
@@ -0,0 +1,56 @@
+print("Hola mundo")
+
+print(2+3)
+print(2-3)
+# Módulo
+print(3 % 2)
+
+res= None
+res = "Hola"##sobrescribir
+print(res)
+
+
+cadena = "Hola 'me llamo Alvaro'"
+print(cadena)
+
+cadena_salto_linea = "Hola\mundo"
+print("Un texto\tuna tabulación")
+print("Un texto\nuna nueva línea")
+
+
+
+print(r"C:\nombre\directorio")  # r(cadena) => raw (cruda)
+
+diez_espacios = " " * 10
+print(diez_espacios + "un texto a diez espacios")
+
+palabra = "Python"
+palabra[0] # carácter en la posición 0
+print(palabra[3])
+
+palabra2 = "Spring"
+print (palabra2[0:2])
+
+print (palabra2[:2])
+
+##inmutabilidad
+palabra = "N" + palabra[1:]
+palabra
+
+
+#funciones
+def nombre_funcion(parametro1):
+
+    return parametro1
+
+
+
+palabra = "Python"
+len(palabra)
+
+##listas
+
+
+
+
+
